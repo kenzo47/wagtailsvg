6d44d52db0929616b843219598d19865f2d978e2
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2024-05-06 17:23:17 +0200

    Update setup script to use newer version of wagtail-generic-chooser.

--- setup.py
+++ setup.py
@@ -8,35 +8,33 @@ PROJECT_DIR = os.path.dirname(__file__)
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
-with open('./package.json') as package:
+with open("./package.json") as package:
     data = json.load(package)
-    version = data['version']
+    version = data["version"]
 
 setup(
-    name='wagtailsvg',
+    name="wagtailsvg",
     version=version,
-    url='https://github.com/Aleksi44/wagtailsvg',
+    url="https://github.com/Aleksi44/wagtailsvg",
     author="Alexis Le Baron",
     author_email="alexis@stationspatiale.com",
     description="Wagtail SVG",
     long_description=long_description,
     keywords="wagtail svg",
-    license='GPL-3.0',
-    install_requires=[
-        'wagtail-generic-chooser==0.5.1'
-    ],
-    platforms=['linux'],
+    license="GPL-3.0",
+    install_requires=["wagtail-generic-chooser==0.6.1"],
+    platforms=["linux"],
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development'
-    ]
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+    ],
 )
